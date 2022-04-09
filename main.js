0aafa155112f4395af763ae93ba7a288bda8af82
chrism-48 <moore4020@gmail.com>
2022-04-08 20:42:59 -0400

    starting on logic

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
 
-const myThings = ['rock', 'paper', 'scissors'];
-const computerPlay = myThings[Math.floor(Math.random() * myThings.length)];
-console.log(computerPlay);
+console.log("Let's play a game of rock paper scissors!")
+console.log("Enter rock paper or scissors into prompt.")
+function playerSelection() {
+    const gameQuestion = prompt("Enter your choice.").toLowerCase();
+    console.log(`You have chosen ${gameQuestion}.`);
+    return gameQuestion;
+    
+    
+}
+
+
+function computerSelection(gameQuestion) {    
+    const gameVar = ['rock', 'paper', 'scissors'];
+    const randomItem = gameVar[Math.floor(Math.random() * gameVar.length)];
+    console.log(`Computer has chosen ${randomItem}.`);
+    return randomItem;
+        
+}
+
+
+function playRound(randomItem,gameQuestion) {
+    console.log("I'm here")
+    
+}
+
+
+// Run program //
+
+playerSelection();
+computerSelection();
+playRound();
+
+
+
+
