9db59cd05899f9f43e886fb3c4086a7d92c9ece4
chrism-48 <moore4020@gmail.com>
2022-04-09 11:15:39 -0400

    console version working.

--- main.js
+++ main.js
@@ -1,35 +1,75 @@
+//----------------------------------------------------------//
+//   A game of rock paper scissors by:Chris Moore 2022      //
+//----------------------------------------------------------//
 
-console.log("Let's play a game of rock paper scissors!")
-console.log("Enter rock paper or scissors into prompt.")
+
+
+// -------------- initialize variables -------------------- //
+
+let gameQuestion;
+let randomItem;
+let gameVar;
+
+
+// -------------------- get user input -------------------- //
+
 function playerSelection() {
-    const gameQuestion = prompt("Enter your choice.").toLowerCase();
+    gameQuestion = prompt("Enter (rock) or (paper) or (scissors).").toLowerCase();
     console.log(`You have chosen ${gameQuestion}.`);
-    return gameQuestion;
-    
-    
+    return gameQuestion;   
+
 }
 
 
-function computerSelection(gameQuestion) {    
-    const gameVar = ['rock', 'paper', 'scissors'];
-    const randomItem = gameVar[Math.floor(Math.random() * gameVar.length)];
+// --------------- get computer random input ------------- //
+
+function computerSelection() {    
+    gameVar = ['rock', 'paper', 'scissors'];
+    randomItem = gameVar[Math.floor(Math.random() * gameVar.length)];
     console.log(`Computer has chosen ${randomItem}.`);
     return randomItem;
         
 }
 
 
-function playRound(randomItem,gameQuestion) {
-    console.log("I'm here")
-    
+// -------------- Find the winner of the game ------------- //
+
+function playRound(gameQuestion, randomItem) {   
+    if (gameQuestion === 'rock' && randomItem === 'rock') {
+        console.log('It is a tie!');
+    } else if (gameQuestion === 'rock' && randomItem === 'paper') {
+        console.log('Paper covers rock, computer wins.');
+    } else if (gameQuestion === 'rock' && randomItem === 'scissors') {
+        console.log('Rock breaks scissors, you win!');
+    } else if (gameQuestion === 'paper' && randomItem === 'rock') {
+        console.log('Paper covers rock, you win!');
+    } else if (gameQuestion === 'paper' && randomItem === 'paper') {
+        console.log('It is a tie.');
+    } else if (gameQuestion === 'paper' && randomItem === 'scissors') {
+        console.log('Scissors cuts paper, computer wins.');
+    } else if (gameQuestion === 'scissors' && randomItem === 'rock') {
+        console.log('Rock breaks scissors, computer wins.');
+    } else if (gameQuestion === 'scissors' && randomItem === 'paper') {
+        console.log('Scissors cuts paper, you win!');
+    } else if (gameQuestion === 'scissors' && randomItem === 'scissors') {
+        console.log('It is a tie.');
+    }     
 }
 
+// ------------------- Reset program ---------------------- //
 
-// Run program //
+function clear() {
+    prompt("Click OK to play again.")
+    location.reload();    
+}
 
+// ---------------------- Run program --------------------- //
+console.log("Let's play a game of rock paper scissors!")
+console.log("Enter rock paper or scissors into prompt.")
 playerSelection();
 computerSelection();
-playRound();
+playRound(gameQuestion,randomItem);
+clear()
 
 
 
